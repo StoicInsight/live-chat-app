7779173b3da0b1d0cc8dd44be75ddf12e96436d4
StoicInsight <gettantolcen@gmail.com>
2023-02-22 12:09:45 -0500

    styled message component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+        <div className="userChatInfo">
+          <span>Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+        <div className="userChatInfo">
+          <span>Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+        <div className="userChatInfo">
+          <span>Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+        <div className="userChatInfo">
+          <span>Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
